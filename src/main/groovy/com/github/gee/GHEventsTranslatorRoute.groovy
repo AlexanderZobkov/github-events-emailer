70e0f43638674654c7522ecd4514b561b30fcbf0
AlexanderZobkov <alexander.zobkov@gmail.com>
2020-11-25 02:48:30 +0300

    Implement filtering by name/type of github event

--- src/main/groovy/com/github/gee/GHEventsTranslatorRoute.groovy
+++ src/main/groovy/com/github/gee/GHEventsTranslatorRoute.groovy
@@ -1,8 +1,12 @@
 package com.github.gee
 
 import groovy.transform.CompileStatic
+import org.apache.camel.Exchange
 import org.apache.camel.Expression
+import org.apache.camel.Processor
 import org.apache.camel.builder.RouteBuilder
+import org.kohsuke.github.GHEventPayload
+import org.kohsuke.github.GitHub
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
@@ -15,14 +19,35 @@ import org.springframework.stereotype.Component
 class GHEventsTranslatorRoute extends RouteBuilder {
 
     @Autowired
+    GitHub gitHub
+
+    @Autowired
     @Qualifier(value = 'translator')
     Expression translator
 
     @Override
     void configure() throws Exception {
         from('seda:github-events').id('translator')
+                .process(unmarshallEvent()).id('unmarshall-event')
                 .transform(translator).id('translate-github-events')
                 .to('seda:email-sender').id('to-email-sender')
+    }
+
+    Processor unmarshallEvent() {
+        return new Processor() {
+            void process(Exchange exchange) throws Exception {
+                String eventType = exchange.in.headers['X-GitHub-Event']
+                exchange.in.body = gitHub.parseEventPayload(
+                        exchange.in.getMandatoryBody(Reader),
+                        eventToClass(eventType))
+            }
+            private Class<GHEventPayload> eventToClass(String event) {
+                return this.
+                        class.
+                        classLoader.
+                        loadClass('org.kohsuke.github.GHEventPayload$' + event.capitalize())  as Class<GHEventPayload>
+            }
+        }
     }
 
 }
--- src/main/groovy/com/github/gee/WebHookReceiverRoute.groovy
+++ src/main/groovy/com/github/gee/WebHookReceiverRoute.groovy
@@ -1,27 +1,19 @@
 package com.github.gee
 
 import groovy.transform.CompileStatic
-import org.apache.camel.Exchange
 import org.apache.camel.ExchangePattern
-import org.apache.camel.Expression
-import org.apache.camel.LoggingLevel
-import org.apache.camel.Processor
+import org.apache.camel.Message
 import org.apache.camel.builder.RouteBuilder
-import org.kohsuke.github.GHEventPayload
-import org.kohsuke.github.GitHub
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 /**
- * A route that listens for calls from Github hooks.
+ * A route that listens for calls from Github webhooks.
  */
 @CompileStatic
 @Component
+@SuppressWarnings('DuplicateStringLiteral')
 class WebHookReceiverRoute extends RouteBuilder {
-
-    @Autowired
-    GitHub gitHub
 
     @Value('${webhook.listen.address}')
     String webhookListenAddress
@@ -29,46 +21,29 @@ class WebHookReceiverRoute extends RouteBuilder {
     @Value('${webhook.listen.port}')
     int webhookListenPort
 
-    @SuppressWarnings('GStringExpressionWithinString')
+    @Value('${webhook.events.accept}')
+    List<String> eventsToAccept
+
     @Override
     void configure() throws Exception {
         from("jetty:http://${webhookListenAddress}:${webhookListenPort}?matchOnUriPrefix=true")
                 .routeId('github-webhook')
-                .process(unmarshallEvent()).id('unmarshall-event')
                 .choice().id('events-filter')
-                    .when().expression(shouldPassEvent())
+                    .when().message(shouldPass)
                         .to(ExchangePattern.InOnly, 'seda:github-events').id('to-github-events')
                     .otherwise()
-                        .log(LoggingLevel.WARN, 'Ignoring event: ${header[X-GitHub-Event]}').id('log-ignored-event')
+                        .process().message(logIgnoredEvent).id('log-ignored-event')
                 .end()
         .removeHeaders('*').id('remove-headers-replying-webhook')
         .setBody().constant('').id('set-webhook-reply')
     }
 
-    Processor unmarshallEvent() {
-        return new Processor() {
-            void process(Exchange exchange) throws Exception {
-                String eventType = exchange.in.headers['X-GitHub-Event']
-                exchange.in.body = gitHub.parseEventPayload(
-                        exchange.in.getMandatoryBody(Reader),
-                        eventToClass(eventType))
-            }
-            private Class<GHEventPayload> eventToClass(String event) {
-                return this.
-                        class.
-                        classLoader.
-                        loadClass('org.kohsuke.github.GHEventPayload$' + event.capitalize())  as Class<GHEventPayload>
-            }
-        }
+    Closure<Boolean> shouldPass = { Message message ->
+        eventsToAccept.contains('*') ?: String.cast(message.headers['X-GitHub-Event']) in eventsToAccept
     }
 
-    Expression shouldPassEvent() {
-        return new Expression() {
-            def <T> T evaluate(Exchange exchange, Class<T> type) {
-                return type.cast([GHEventPayload.Push,
-                           GHEventPayload.Create,
-                           GHEventPayload.Delete].contains(exchange.in.body.class))
-            }
-        }
+    Closure<Void> logIgnoredEvent = { Message message ->
+        log.warn("Ignoring event: ${message.headers['X-GitHub-Event']}, " +
+                "accepting: ${eventsToAccept.join(', ')}")
     }
 }
